6dbdbb277d05df007db175a575224c39e68292ee
Ikram Bk <bk-ikram@hotmail.com>
2025-10-18 15:40:35 -0400

    fixed methods to add ships, and to display grid.

--- classes.js
+++ classes.js
@@ -29,12 +29,52 @@ class Gameboard{
         let grid = new Array();
         for(let i = 0; i<10 ; i++)
            grid.push(new Array(10));
-        console.log(grid);
         return grid
     }
-    placeShip(){
+    placeShip(size,startPoint,direction){
+        //IMPORTANT: points are denoted by [y,x];
+        //Check that the direction passed is valid
+        if(!['right','down'].includes(direction))
+            throw new Error('Wrong direction passed');
+        //Check that the ship position does not go past
+        //the gameboard boundaries, and does not superimpose 
+        //with an existing ship
+
+        //Depending on the given direction, we will either vary the x 
+        // or the y coordinate
+        const startX = startPoint[1];
+        const startY = startPoint[0];
+        const start = direction === 'right' ? startX : startY;
+        let positions = [];
+        for(let i = start; i < start + size ; i++){
+            if(i < 0 || i > 9)
+                return false;
+            const p 
+                = direction === 'right' 
+                    ? [startY, i]
+                    : [i, startX];
+            if(! (this.grid[p[0]][p[1]] === undefined)){
+                throw new Error("Another ship already exists there");
+            }
+            else
+                positions.push(p);
+        }
+        const ship = new Ship(size);
+        //console.log(positions);
+        positions.forEach((ele) => this.grid[ele[0]][ele[1]] = ship);
+        return true;
 
     }
+    get gridVisual(){
+        const visual = structuredClone(this.grid);
+        for(let y = 0; y < visual.length ; y++){
+            const horizontal = visual[y];
+            for (let x = 0; x < horizontal.length ; x++)
+                visual[y][x] = visual[y][x] === undefined ? '0' : 'X';
+        }
+        return visual;
+    }
+
 }
 
 function areDeeplyEqual(arr1, arr2){
--- classes.test.js
+++ classes.test.js
@@ -1,4 +1,11 @@
+import util from 'util';
 import {Ship, Gameboard, areDeeplyEqual} from './classes.js';
+
+//to allow full width console out.
+
+
+util.inspect.defaultOptions.breakLength = Infinity;
+util.inspect.defaultOptions.maxArrayLength = null;
 
 describe('Ship', () => {
     const ship = new Ship(4);
@@ -20,22 +27,79 @@ describe('Ship', () => {
 })
 
 describe('Gameboard', () => {
-    test('initializes grid with correct ship placements',()=>{
-        const gameboard = new Gameboard();
-        gameboard.placeShip(4,[0,2],'right');
-        result = gameboard.grid;
+    const gameboard = new Gameboard();
+    test('initializes grid correctly',()=>{
+        result = gameboard.gridVisual;
         expected= [
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-            [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined] 
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'] 
         ]
-        
+        expect(areDeeplyEqual(result,expected)).toBe(true);
+    })
+    test('grid updated correctly with ship added horizontally',()=>{
+        gameboard.placeShip(4,[0,2],'right');
+        result = gameboard.gridVisual;
+        //result.forEach((line) => console.log(JSON.stringify(line)));
+        expected= [
+            ['0','0','X','X','X','X','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'] 
+        ]
+        expect(areDeeplyEqual(result,expected)).toBe(true);
+    });
+    test('grid updated correctly with ship added vertically',()=>{
+        gameboard.placeShip(2,[3,9],'down');
+        result = gameboard.gridVisual;
+        //result.forEach((line) => console.log(JSON.stringify(line)));
+        expected= [
+            ['0','0','X','X','X','X','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','X'],
+            ['0','0','0','0','0','0','0','0','0','X'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'] 
+        ]
+        expect(areDeeplyEqual(result,expected)).toBe(true);
+    })
+
+    test('error thrown when trying to place ship in an occupied spot.',()=>{
+        expect(() => gameboard.placeShip(2,[3,9],'down')).toThrow();
+    })
+
+    test('check, after being unable to place ship, that the grid did not change.',()=>{
+        result = gameboard.gridVisual;
+        //result.forEach((line) => console.log(JSON.stringify(line)));
+        expected= [
+            ['0','0','X','X','X','X','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','X'],
+            ['0','0','0','0','0','0','0','0','0','X'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0'] 
+        ]
+        expect(areDeeplyEqual(result,expected)).toBe(true);
     })
 })
